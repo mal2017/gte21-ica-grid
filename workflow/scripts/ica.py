15c8810024d5c1975e64f6fc841544abe0c62399
mal2017 <matt.a.lawlor@gmail.com>
2021-02-25 15:26:40 -0500

    random wait - working on ftputil error

--- workflow/scripts/ica.py
+++ workflow/scripts/ica.py
@@ -1,12 +1,19 @@
 from random import randint
 from time import sleep
 
+# running into ftputil errors, one of the below libraries must check with an ftp site
+# for updates. TO deal with this I have the script sleep a random interval
+# after each module loads. This seems to massively improve the issue.
+# Ideally there would be a slurm flag that makes sure jobs aren't submitted
+# all at once, but I can't find anything like it.
 sleep(randint(1,60))
 
-
 import pandas as pd
+sleep(randint(1,60))
 import numpy as np
+sleep(randint(1,60))
 import random
+sleep(randint(1,60))
 from sklearn.decomposition import FastICA
 
 seed = snakemake.params['random_seed']
